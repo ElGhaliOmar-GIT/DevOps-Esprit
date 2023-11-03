b2689ffeb0fcf9900bc32ef2b366fa4d04e4789c
SiwarBaccouche23 <siwar.baccouche@esprit.tn>
2023-11-04 00:37:15 +0100

    FirstCommit(TestJUNITClasseDepartement)

---
+++ src/test/java/DepartementTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+import tn.esprit.spring.kaddem.entities.Departement;
+import static org.junit.Assert.*;
+
+public class DepartementTest {
+
+        @Test
+        public void testConstructor() {
+            Departement departement = new Departement();
+            assertNotNull(departement);
+        }
+
+        @Test
+        public void testParameterizedConstructor() {
+            String nomDepart = "Informatique";
+            Departement departement = new Departement(nomDepart);
+            assertNotNull(departement);
+            assertEquals(nomDepart, departement.getNomDepart());
+        }
+
+        @Test
+        public void testGettersAndSetters() {
+            Departement departement = new Departement();
+
+            Integer idDepart = 1;
+            departement.setIdDepart(idDepart);
+            assertEquals(idDepart, departement.getIdDepart());
+
+            String nomDepart = "Mathématiques";
+            departement.setNomDepart(nomDepart);
+            assertEquals(nomDepart, departement.getNomDepart());
+
+
+        }
+    }
+
