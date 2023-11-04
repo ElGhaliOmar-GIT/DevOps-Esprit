053bb7e75acaec5fa882fac63381fc9705a0d5cc
SiwarBaccouche23 <siwar.baccouche@esprit.tn>
2023-11-04 01:56:05 +0100

    Commit(Nexus)

--- src/test/java/DepartementTest.java
+++ src/test/java/DepartementTest.java
@@ -1,36 +1,38 @@
 import org.junit.jupiter.api.Test;
 import tn.esprit.spring.kaddem.entities.Departement;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DepartementTest {
 
-        @Test
-        public void testConstructor() {
-            Departement departement = new Departement();
-            assertNotNull(departement);
-        }
-
-        @Test
-        public void testParameterizedConstructor() {
-            String nomDepart = "Informatique";
-            Departement departement = new Departement(nomDepart);
-            assertNotNull(departement);
-            assertEquals(nomDepart, departement.getNomDepart());
-        }
-
-        @Test
-        public void testGettersAndSetters() {
-            Departement departement = new Departement();
-
-            Integer idDepart = 1;
-            departement.setIdDepart(idDepart);
-            assertEquals(idDepart, departement.getIdDepart());
-
-            String nomDepart = "Mathématiques";
-            departement.setNomDepart(nomDepart);
-            assertEquals(nomDepart, departement.getNomDepart());
-
-
-        }
+    @Test
+    public void testConstructor() {
+        Departement departement = new Departement();
+        assertNotNull(departement);
     }
+
+    @Test
+    public void testParameterizedConstructor() {
+        String nomDepart = "Informatique";
+        Departement departement = new Departement(nomDepart);
+        assertNotNull(departement);
+        assertEquals(nomDepart, departement.getNomDepart());
+    }
+
+    @Test
+    public void testGettersAndSetters() {
+        Departement departement = new Departement();
+
+        Integer idDepart = 1;
+        departement.setIdDepart(idDepart);
+        assertEquals(idDepart, departement.getIdDepart());
+
+        String nomDepart = "Mathématiques";
+        departement.setNomDepart(nomDepart);
+        assertEquals(nomDepart, departement.getNomDepart());
+
+
+    }
+}
+
+
 
